6204989113a758e60fff4bac4e09c4b273fe1177
Web Guy <webguywork@gmail.com>
2026-01-04 15:17:00 -0700

    0.1

--- assets/javascripts/discourse/pages-route-map.js
+++ assets/javascripts/discourse/pages-route-map.js
@@ -1,6 +1,9 @@
 export default {
 	resource: "admin.adminPlugins",
 	map() {
-		this.route("pages");
+		this.route("pages", function() {
+			this.route("new");
+			this.route("edit", { path: "/:id/edit" });
+		});
 	},
 };
